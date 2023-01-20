e0869def9d4ca23f509efec714c31c68a7097319
trynceb <tryncebrandt@gmail.com>
2023-01-20 10:15:05 -0600

    Add file structure and setup OAuth

---
+++ eventtracker/main_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Goes in main model to link to user
+# user = models.ForeignKey(User, on_delete=models.CASCADE)
